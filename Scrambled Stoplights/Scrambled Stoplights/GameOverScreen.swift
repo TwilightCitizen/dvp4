1cfbdaf7dd3125aa2c6fb9680ee398d461147a38
David Clark <daclark1@student.fullsail.edu>
2019-09-21 10:49:11 -0400

    Cleaned up GameOverScreen with comments and fixed some indentation.

--- Scrambled Stoplights/Scrambled Stoplights/GameOverScreen.swift
+++ Scrambled Stoplights/Scrambled Stoplights/GameOverScreen.swift
@@ -11,14 +11,17 @@ import UIKit
 class GameOverScreen : UIViewController {
     // Outlets
     
+    // Elements that need corners rounded
     @IBOutlet var roundAllCorners : [ UIView ]!
     
+    // Required for reporting various game statistics after game over
     @IBOutlet weak var scoreLabel   : UILabel!
     @IBOutlet weak var clearsLabel  : UILabel!
     @IBOutlet weak var bestRunLabel : UILabel!
     
     // Properties
     
+    // Game statistics passed in during segue from game screen
     var score   : Int!
     var clears  : Int!
     var bestRun : Int!
@@ -30,6 +33,7 @@ class GameOverScreen : UIViewController {
         
         roundCorners()
         
+        // Load the statistics into the "modal" view
         scoreLabel.text   = score.withCommas
         clearsLabel.text  = clears.withCommas
         bestRunLabel.text = bestRun.withCommas
@@ -37,10 +41,11 @@ class GameOverScreen : UIViewController {
     
     func roundCorners() { roundAllCorners.forEach { $0.round() } }
     
-    @IBAction func tappedOutsideOfModal( _ sender : UITapGestureRecognizer ) {
-        dismiss( animated : true, completion : nil )
-    }
+    // Dismiss the game over screen when user taps outside of the "modal" region
+    @IBAction func tappedOutsideOfModal( _ sender : UITapGestureRecognizer ) { dismiss( animated : true, completion : nil ) }
     
+    // Dismiss the game over screen and segue to the leaderboard on behalf of the
+    // game screen if the user taps the leaderboard button
     @IBAction func leaderboardTapped( _ sender : UIButton ) {
         let vc = presentingViewController!.children.last! as! GameScreen
         
